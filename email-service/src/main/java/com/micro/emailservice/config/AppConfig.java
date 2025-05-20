7c2c7050bda6b78373482edadcf70036f5f9ef55
shibeiinu3110 <huyhungshiba@gmail.com>
2025-05-20 13:49:12 +0700

    fix: use environment variables & constructor injections

--- email-service/src/main/java/com/micro/emailservice/config/AppConfig.java
+++ email-service/src/main/java/com/micro/emailservice/config/AppConfig.java
@@ -2,14 +2,21 @@ package com.micro.emailservice.config;
 
 
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.sendgrid.SendGrid;
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
-    @Value("${spring.sendgrid.api-key}")
+    private final MailProperties mailProperties;
     private String sendGridApiKey;
+    @PostConstruct
+    void init() {
+        this.sendGridApiKey= mailProperties.getApiKey();
+    }
 
     @Bean
     public SendGrid sendGrid() {
